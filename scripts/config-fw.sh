d186362044b902d626ab6f0ec971c22d27a0d9e7
Bruno <brunopolicarpo@hotmail.com>
2020-05-01 16:04:44 -0300

    commit-fw.sh output seems to be working?

--- scripts/config-fw.sh
+++ scripts/config-fw.sh
@@ -10,13 +10,34 @@ sudo iptables -P OUTPUT DROP
 sudo iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT
 sudo iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
 
+if [ $debug -eq 1 ]; then 
+    sudo iptables -t raw -A PREROUTING -p tcp --dport 53 -j TRACE
+    sudo iptables -t raw -A PREROUTING -p udp --dport 53 -j TRACE
+    sudo iptables -t raw -A OUTPUT -p tcp --dport 53 -j TRACE
+    sudo iptables -t raw -A OUTPUT -p udp --dport 53 -j TRACE
+fi
+
 # HTTP
 sudo iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
 sudo iptables -A OUTPUT -p udp --dport 80 -j ACCEPT
 
+if [ $debug -eq 1 ]; then 
+    sudo iptables -t raw -A PREROUTING -p tcp --dport 80 -j TRACE
+    sudo iptables -t raw -A PREROUTING -p udp --dport 80 -j TRACE
+    sudo iptables -t raw -A OUTPUT -p tcp --dport 80 -j TRACE
+    sudo iptables -t raw -A OUTPUT -p udp --dport 80 -j TRACE
+fi
+
 # HTTPS
 sudo iptables -A OUTPUT -p tcp --dport 443 -j ACCEPT
 sudo iptables -A OUTPUT -p udp --dport 443 -j ACCEPT
+
+if [ $debug -eq 1 ]; then 
+    sudo iptables -t raw -A PREROUTING -p tcp --dport 443 -j TRACE
+    sudo iptables -t raw -A PREROUTING -p udp --dport 443 -j TRACE
+    sudo iptables -t raw -A OUTPUT -p tcp --dport 443 -j TRACE
+    sudo iptables -t raw -A OUTPUT -p udp --dport 443 -j TRACE
+fi
 
 # conexoes feitas sao aceitas
 sudo iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
