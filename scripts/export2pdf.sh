3b7d20935fb736cd013cf25c1ab1d2e44a5782bb
Bruno <brunopolicarpo@hotmail.com>
2020-05-03 22:44:50 -0300

    export2pdf.sh now allows compression of generated files
    TODO: allow folder structure copying

--- scripts/export2pdf.sh
+++ scripts/export2pdf.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-## DESTDIR=/var/www/html/SISOP/
-## DESTDIR=$HOME/testeExportacao
-
 POSITIONAL=()
 
 FORCE_REMOVE=0 # empties directory if it already exists
-FORMAT=odp # chooses which libreoffice format to convert to pdf
+COPY_FOLDERS=0 # copy selected folder structures
+COMPRESS=0 # compresses directory with converted pdfs
+EXT=odp # chooses which libreoffice extention to convert to pdf
 LOG=/dev/null # default log file
 
 while [[ $# -gt 0 ]]
@@ -14,28 +13,48 @@ do
     key="$1"
 
     case $key in
+		# folder to save converted pdfs
         --outdir|-o)
         DESTDIR="$2"
         shift # past argument
         shift # past value
         ;;
+
+		# folder to search for files to convert
         --srcdir|-i)
         SRCDIR="$2"
         shift # past argument
         shift # past value
         ;;
-		-f)
+
+		--force|-f)
 		FORCE_REMOVE=1
-		;;
-		--format)
-		FORMAT=$2
 		shift
 		;;
-        ##--default)
-        ##DEFAULT=YES
-        ##shift # past argument
-        ##;;
-        *)    # unknown option
+
+		# select what extension to convert
+		--ext|-e)
+		EXT=$2
+		shift # past argument
+		shift # past value
+		;;
+
+		# compress pdf folder after extraction
+		--zip|-z)
+		COMPRESS=1
+		shift
+		;;
+
+		# copy folder structure
+		--dir|-d)
+		COPY_FOLDERS=1
+		FOLDER=$2
+		shift # past argument
+		shift # past value
+		;;
+
+		# unknown option
+        *)    
         POSITIONAL+=("$1") # save it in an array for later
         shift # past argument
         ;;
@@ -48,7 +67,6 @@ set -- "${POSITIONAL[@]}" # restore positional parameters
 ################################
 
 # checking SRCDIR
-
 if [ ! -d "${SRCDIR}" ]; then
 	echo "${SRCDIR} not a directory - aborting"
 	exit
@@ -57,44 +75,50 @@ fi
 echo "SRCDIR: ${SRCDIR}"
 
 # checking DESTDIR
-
 if [ "${DESTDIR}" == "" ]; then 
 	echo "${DESTDIR} not set - aborting"
 	exit -1
 else
-	if [ ! -d "$DESTDIR" ]; then 
-		#directory does not exist - creating it
+	if [ ! -d "${DESTDIR}" ]; then
 
-		echo "creating $DESTDIR ..."
+		# directory does not exist - creating it
+		echo "Creating $DESTDIR ..."
 		mkdir -p "$DESTDIR" 2>&1 > ${LOG}
 		if [ $? -ne 0 ]; then
-			echo "error creating \"$DESTDIR\""
+			echo "Error creating \"$DESTDIR\" - aborting"
 			exit
 		fi
 	else
+
 		# directory exists
-	
 		if [ `ls "${DESTDIR}" | wc -l` -ne 0 ]; then
+
 			# directory exists and is not empty, check if it is marked to be removed
+			echo -n "Directory ${DESTDIR} not empty"
 
 			if [ $FORCE_REMOVE -eq 1 ]; then
-				echo "Emptying ${DESTDIR} ..."
-				rm -rf "${DESTDIR}/*" 2>&1 > ${LOG}
+				echo "- erasing old contents"
+				rm -rf "${DESTDIR}" 2>&1 > ${LOG}
+
+				if [ $? -eq 0 ]; then 
+					mkdir -p ${DESTDIR}
+				else
+					exit 1
+				fi  
 			else
-				echo "Directory ${DESTDIR} not empty - aborting"
-				exit
+				echo "- aborting"
+				exit 1
 			fi
-		else 
-			echo "Directory ${DESTDIR} already exists, not empty"
+		else
+			echo "Directory ${DESTDIR} already exists and is empty"
 		fi
 	fi
 fi
 
 echo "DESTDIR: ${DESTDIR}"
 
-# checking FORMAT
-
-case $FORMAT in
+# checking EXT
+case ${EXT} in
 	odp)
 		program=impress
 		;;
@@ -104,12 +128,12 @@ case $FORMAT in
 		;;
 
 	*)
-		echo "Error: Invalid extension: $FORMAT. Aborting"
+		echo "Error: Invalid extension: $EXT. Aborting"
 		exit
 		;;
 esac
 
-echo -e "Format to export: ${FORMAT}.\nProgram: ${program}"
+echo -e "Extension to export: ${EXT}\nProgram: ${program}"
 
 ###################
 # Starting script #
@@ -120,38 +144,33 @@ pdfdir="`basename $SRCDIR`-PDF"
 echo "Exporting to ${pdfdir}"
 
 # converting to pdf 
-find ${SRCDIR} -name *.${FORMAT} | xargs -I{} libreoffice --${program} --convert-to pdf --outdir "${pdfdir}" {} 2> ${LOG}
+find ${SRCDIR} -name *.${EXT} | \
+	xargs -I{} libreoffice \
+	--${program} \
+	--convert-to pdf \
+	--outdir "${pdfdir}" {} 2> ${LOG}
+
+# compressing files to zip, if needed
+if [ ${COMPRESS} -eq 1 ]; then 
+	echo "Compressing ${pdfdir} folder ..."
+	zip -r "${pdfdir}.zip" "${pdfdir}/" 2>&1 > ${LOG}
+	mv "${pdfdir}.zip" "${DESTDIR}/."
+fi
 
 # moving to DESTDIR
 mv "${pdfdir}" "${DESTDIR}/."
 
-## zip -r Aulas-PPC.zip Aulas-PDF/
+# copy selected folder structure to output dir
+if [ ${COPY_FOLDERS} -eq 1 ]; then
 
-## [ -d "$DESTDIR/Aulas-PDF/" ] && rm -rf "$DESTDIR/Aulas-PDF/"
+	echo "Copying all \"${FOLDER}\" found inside \"${SRCDIR}\""
 
-## mv Aulas-PDF/ $DESTDIR/.
-## mv Aulas-PPC.zip $DESTDIR/.
+	for dir in $(find "${SRCDIR}" -maxdepth 1 -type d); do
 
-# convertendo listas para pdf 
-## find . -name SISOP-Exercicios*.odt | xargs -I{} libreoffice --writer --convert-to pdf --outdir Listas-PDF {}
+		if [ -d "${dir}" ]; then 
+			echo ${dir}
+		fi
 
+	done
 
-if [ 1 -eq 0 ]; then 
-	zip -r Listas-SISOP.zip Listas-PDF/
 fi
-
-## [ -d "$DESTDIR/Listas-PDF/" ] && rm -rf "$DESTDIR/Listas-PDF/"
-
-## mv Listas-PDF/ $DESTDIR/.
-## mv Listas-PPC.zip $DESTDIR/.
-
-# # copiando exemplos 
-# for d in $(find . -type d -maxdepth 1); do
-# 	cd $d;
-
-# 	if [ -d "Exemplos" ]; then 
-# 		echo $d
-# 	fi
-
-# 	cd ..
-# done
