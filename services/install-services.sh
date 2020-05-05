2a24106021d4a030bb113827b7947a5bcfe63e24
Bruno <brunopolicarpo@hotmail.com>
2020-05-05 20:20:09 -0300

    Added script to install daemons Added VPN and Firewall services

--- services/install-services.sh
+++ services/install-services.sh
@@ -2,6 +2,36 @@
 
 DEBUG=1
 
+curdir=`pwd`
+rootdir=`dirname ${curdir}`
+echo "Root dir: ${rootdir}"
+
+for service in $(ls *.service); do
+	echo "Installing ${service} ... "
+
+	sudo systemctl stop ${service}
+	sudo systemctl disable ${service}	
+
+	sed -e "s/SERVICES_FOLDER/${rootdir//\//\\\/}/g" ${service} > /tmp/${service}.tmp
+
+	sudo mv /tmp/${service}.tmp /etc/systemd/system/${service}
+done
+
+sudo systemctl daemon-reload
+
+for service in $(ls *.service); do
+	echo "Enabling ${service} ... "
+	sudo systemctl enable ${service}
+done
+
+for service in $(ls *.service); do
+	echo "Starting ${service} ... "
+	sudo systemctl start ${service}
+done
+
+exit
+
+
 if [ ! -f "$PWD/git-sync.service" ]; then
 	echo "git-sync.service not found - aborting"
 	exit
