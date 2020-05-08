815bfab161fbd80acdc7bfc5c77f98e7e46a1554
Bruno <brunopolicarpo@hotmail.com>
2020-05-08 00:47:48 -0300

    VPN locations can be randomized at startup

--- scripts/config-vpn.sh
+++ scripts/config-vpn.sh
@@ -2,6 +2,14 @@
 
 LOG=/dev/stdout 
 
+if [ -f ${HOME}/.vpn-locations ]; then
+	num_locations=`wc -l ${HOME}/.vpn-locations | awk '{ print $1 }'`
+	shuf ${HOME}/.vpn-locations > ~/.vpn-locations.session
+	cur_location=1
+else 
+	rm -f ~/.vpn-locations.session
+fi
+
 opt="$1"
 
 case $opt in
@@ -9,10 +17,23 @@ case $opt in
 start)
 	timeout=1
 	while :; do
-		windscribe connect # >> $LOG
+		if [ -f ${HOME}/.vpn-locations.session ]; then
+			location=`sed -n "${cur_location}{p;q}" ${HOME}/.vpn-locations.session`
+			[ ${cur_location} -lt ${num_locations} ] &&\
+				cur_location=$(( cur_location + 1 )) 
+				cur_location=1
+			echo "Location: ${location}"
+		else 
+			location=""
+		fi
+		windscribe connect ${location}
 		windscribe status | grep -q CONNECTED
 		if [ $? -eq 0 ]; then
-			echo "`date +%c`: windscribe connected" 2>&1 # >> $LOG 
+			echo "`date +%c`: windscribe connected" 2>&1 # >> $LOG
+
+			## allow only HTTPS outgoing traffic to pass through VPN 
+			## sudo iptables -I OUTPUT 1 -p tcp --dport 443 -j ACCEPT
+			## sudo iptables -I OUTPUT 2 -p udp --dport 443 -j ACCEPT
 			break
 		else
 			sleep ${timeout}
@@ -32,7 +53,8 @@ stop)
 	echo "Finishing service" 2>&1 
 	windscribe disconnect
 	sudo iptables -F OUTPUT
-	sudo iptables -P OUTPUT ACCEPT 
+	sudo iptables -P OUTPUT ACCEPT
+	rm -f ${HOME}/.vpn-locations.session 
 	;;
 
 *)
