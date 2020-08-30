ae065af1b73bba5ee0981b4e846297fde59dd9bc
Bruno <brunopolicarpo@hotmail.com>
2020-08-30 16:06:29 -0300

    Opens selected apps and sites on VPN startup

--- scripts/config-vpn.sh
+++ scripts/config-vpn.sh
@@ -2,8 +2,8 @@
 
 LOG=/dev/stdout 
 
-if [ -f ${HOME}/.vpn-locations ]; then
-	shuf ${HOME}/.vpn-locations > ~/.vpn-locations.session
+if [[ -f ${HOME}/.vpn-locations ]]; then
+	shuf ${HOME}/.vpn-locations | tee ${HOME}/.vpn-locations.session
 	
 	cur_loc=1
 	N_loc=`wc -l ${HOME}/.vpn-locations | awk '{ print $1 }'`
@@ -15,53 +15,56 @@ opt="$1"
 
 case $opt in
 
-start)
-	timeout=1
-	while :; do
-		if [ -f ${HOME}/.vpn-locations.session ]; then
-			location=`sed -n "${cur_loc}{p;q}" ${HOME}/.vpn-locations.session`
-			cur_loc=$(( (cur_loc + 1) % N_loc ))
-			
-			echo "Location: ${location}"
-		else 
-			location=""
-		fi
-		windscribe connect ${location}
-		windscribe status | grep -q CONNECTED
-		if [ $? -eq 0 ]; then
-			echo "`date +%c`: windscribe connected" 2>&1 # >> $LOG
-
-			## allow only HTTPS outgoing traffic to pass through VPN 
-			## sudo iptables -I OUTPUT 1 -p tcp --dport 443 -j ACCEPT
-			## sudo iptables -I OUTPUT 2 -p udp --dport 443 -j ACCEPT
-			break
-		else
-			sleep ${timeout}
-			timeout=$(( timeout * 2 ))
-
-			if [ ${timeout} -gt 32 ]; then 
-				echo "`date +%c`: too many retries. Aborting" 2>&1 # >> $LOG
-				exit -1
+	start)
+		timeout=1
+		while :; do
+			if [[ -f ${HOME}/.vpn-locations.session ]]; then
+				location=`sed -n "${cur_loc}{p;q}" ${HOME}/.vpn-locations.session`
+				cur_loc=$(( (cur_loc + 1) % N_loc ))
+				
+				echo "Location: ${location}"
 			else 
-				echo "`date +%c`: waiting ${timeout}s before retry..." 2>&1 # >> $LOG
+				location=""
 			fi
-		fi
-	done
-	;;
+			windscribe connect ${location}
+			windscribe status | grep -q CONNECTED
+			if [[ $? -eq 0 ]]; then
+				echo "`date +%c`: windscribe connected" 2>&1 # >> $LOG
 
-stop)
-	echo "Finishing service" 2>&1 
-	windscribe disconnect
-	sudo iptables -F OUTPUT
-	sudo iptables -P OUTPUT ACCEPT
-	rm -f ${HOME}/.vpn-locations.session 
-	;;
+				#msteams open
+				#sites open	
+				config-fw.sh open msteams -1 3
+				config-fw.sh open sites -1 3
 
-*)
- 	echo "Invalid option: \"$opt\"" 2>&1
- 	exit -1
- 	;;
+				## allow only HTTPS outgoing traffic to pass through VPN 
+				## sudo iptables -I OUTPUT 1 -p tcp --dport 443 -j ACCEPT
+				## sudo iptables -I OUTPUT 2 -p udp --dport 443 -j ACCEPT
+				break
+			else
+				sleep ${timeout}
+				timeout=$(( timeout * 2 ))
+
+				if [ ${timeout} -gt 32 ]; then 
+					echo "`date +%c`: too many retries. Aborting" 2>&1 # >> $LOG
+					exit -1
+				else 
+					echo "`date +%c`: waiting ${timeout}s before retry..." 2>&1 # >> $LOG
+				fi
+			fi
+		done
+		;;
+
+	stop)
+		echo "Finishing VPN service" 2>&1 
+		windscribe disconnect
+		sudo iptables -F OUTPUT
+		sudo iptables -P OUTPUT ACCEPT
+		msteams close
+		rm -f ${HOME}/.vpn-locations.session 
+		;;
+
+	*)
+		echo "Invalid option: \"$opt\"" 2>&1
+		exit -1
+		;;
 esac
-
-
-
