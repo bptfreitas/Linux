15cd4ff153b26b88e1dd50c3fcb499dc3f2a4fff
Bruno <brunopolicarpo@hotmail.com>
2020-09-07 18:22:38 -0300

    Added performance test on benchmark4fs.sh

--- scripts/benchmark4fs.sh
+++ scripts/benchmark4fs.sh
@@ -1,14 +1,67 @@
 #!/bin/bash
 
-FS_MOUNT_POINT=/media/VMs # mount point to create filesystems
-TOTAL_SMALL_FILES=10 # total files to create in each test
+#MOUNT_POINT=/media/VMs # mount point to -create filesystems
+
+DEVICE=""
+MOUNT_POINT=""
+
+TOTAL_SMALL_FILES=50000
 TOTAL_LARGE_FILES=${TOTAL_SMALL_FILES}
+
 LARGE_FILE_SZ=$((4*1024*1024)) # size in bytes
 BUFFER_SZ=512
 BLOCK_QTD=$(( LARGE_FILE_SZ / BUFFER_SZ ))
 
-fsSzB4_sm_wr_bytes="`du -sh ${FS_MOUNT_POINT} 2> /dev/null | awk '{ print $1 }'`"
-fsSzB4_sm_wr_blocks="`du -s ${FS_MOUNT_POINT} 2> /dev/null | awk '{ print $1 }'`"
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case $key in
+        --device)
+            # mount point to create filesystems
+            DEVICE=$2            
+            shift # past argument
+            shift # past value
+            ;;
+
+		# configures an output filter as well
+        --mount-point)
+            # mount point to create filesystems
+            MOUNT_POINT=$2            
+            shift # past argument
+            shift # past value
+            ;;
+
+        --number-of-files)
+            TOTAL_SMALL_FILES=$2 # total files to create in each test
+            shift # past argument
+            shift # past value
+            ;;
+
+		# unknown option
+        *)    
+            POSITIONAL+=("$1") # save it in an array for later
+            shift # past argument
+            ;;
+    esac
+done
+
+[[ ! -b $DEVICE ]] &&\
+    { echo "Invalid device: $DEVICE" ; exit -1; }
+
+[[ ! -d $MOUNT_POINT ]] &&\
+    { echo "Invalid mount point: $MOUNT_POINT" ; exit -1; }
+
+mount -l | egrep "${DEVICE}" | egrep -q "${MOUNT_POINT}"
+
+[[ $? -ne 0 ]] &&\
+    { echo "${DEVICE} not mounted on ${MOUNT_POINT}" ; exit -1; } 
+
+[[ ! -w "${MOUNT_POINT}" ]] &&\
+    { echo "Can't write in ${MOUNT_POINT}"; exit -1; }
+
+fsSzB4_sm_wr_bytes="`du -sh ${MOUNT_POINT} 2> /dev/null | awk '{ print $1 }'`"
+fsSzB4_sm_wr_blocks="`du -s ${MOUNT_POINT} 2> /dev/null | awk '{ print $1 }'`"
 
 echo "Filesystem size before writing starting benchmarks (bytes): ${fsSzB4_sm_wr_bytes}"
 echo "Filesystem size before writing starting benchmarks (blocks): ${fsSzB4_sm_wr_blocks}"
@@ -17,13 +70,13 @@ echo -e "\n* Starting filesystem small file creation benchmark *"
 
 { time for i in $(seq 1 ${TOTAL_SMALL_FILES})
 do
-echo "teste$i" > ${FS_MOUNT_POINT}/teste$i.dat
+echo "teste$i" > ${MOUNT_POINT}/teste$i.dat
 done } 2> results_small_wr.tmp
 
 awk ' { if (NR==2) print $2 } ' results_small_wr.tmp
 
-fsSzAfter_sm_wr_bytes="`du -sh ${FS_MOUNT_POINT} 2> /dev/null | awk '{ print $1 }'`"
-fsSzAfter_sm_wr_blocks="`du -s ${FS_MOUNT_POINT} 2> /dev/null | awk '{ print $1 }'`"
+fsSzAfter_sm_wr_bytes="`du -sh ${MOUNT_POINT} 2> /dev/null | awk '{ print $1 }'`"
+fsSzAfter_sm_wr_blocks="`du -s ${MOUNT_POINT} 2> /dev/null | awk '{ print $1 }'`"
 
 echo "Filesystem size after writing small files (bytes): ${fsSzAfter_sm_wr_bytes}"
 echo "Filesystem size after writing small files (blocks): ${fsSzAfter_sm_wr_blocks}"
@@ -31,46 +84,87 @@ echo "Filesystem size after writing small files (blocks): ${fsSzAfter_sm_wr_bloc
 echo -e "\n* Starting filesystem small file deletion benchmark *"
 { time for i in $(seq 1 ${TOTAL_SMALL_FILES})
 do
-rm ${FS_MOUNT_POINT}/teste$i.dat
+rm ${MOUNT_POINT}/teste$i.dat
 done } 2> results_small_del.tmp
 
 awk ' { if (NR==2) print $2 } ' results_small_del.tmp
 
-fsSzAfter_sm_del_bytes="`du -sh ${FS_MOUNT_POINT} 2> /dev/null | awk '{ print $1 }'`"
-fsSzAfter_sm_del_blocks="`du -s ${FS_MOUNT_POINT} 2> /dev/null | awk '{ print $1 }'`"
+fsSzAfter_sm_del_bytes="`du -sh ${MOUNT_POINT} 2> /dev/null | awk '{ print $1 }'`"
+fsSzAfter_sm_del_blocks="`du -s ${MOUNT_POINT} 2> /dev/null | awk '{ print $1 }'`"
 
 echo "Filesystem size after erasing small files (bytes): ${fsSzAfter_sm_del_bytes}"
 echo "Filesystem size after erasing small files (blocks): ${fsSzAfter_sm_del_blocks}"
 
-src_code="
-#include <stdio.h>
-#include <stdlib.h>
+# src_code="
+# #include <stdio.h>
+# #include <stdlib.h>
 
-int main(int argc, const char **argv){
-    FILE *fd = NULL;
-    int i,n,fn;
-    char buffer[${BUFFER_SZ}],filename[32];
+# int main(int argc, const char **argv){
+#     FILE *fd = NULL;
+#     int i,n,fn;
+#     char buffer[${BUFFER_SZ}],filename[32];
 
-    if (argc==1){
-        return -1;
-    }
+#     if (argc==1){
+#         return -1;
+#     }
 
-    fn = atoi(argv[1]);
+#     fn = atoi(argv[1]);
 
-    n = sprintf(filename,\"$FS_MOUNT_POINT/teste%d\",fn);
+#     n = sprintf(filename,\"$MOUNT_POINT/teste%d\",fn);
 
-    fd = fopen(filename, \"w\");
+#     fd = fopen(filename, \"w\");
 
-    for ( i = 0; i < ${BUFFER_SZ}; buffer[i++] = 'X' );
+#     for ( i = 0; i < ${BUFFER_SZ}; buffer[i++] = 'X' );
 
-    for ( i = 0; i < ${BLOCK_QTD} ; i++ ){
-        fwrite( buffer,  sizeof(char),  ${BUFFER_SZ} , fd );
-    }
+#     for ( i = 0; i < ${BLOCK_QTD} ; i++ ){
+#         fwrite( buffer,  sizeof(char),  ${BUFFER_SZ} , fd );
+#     }
 
-    fclose(fd);
-}
-";
+#     fclose(fd);
+# }
+# ";
 
-echo "${src_code}" > /tmp/main.c
-gcc /tmp/main.c -o /tmp/largeFileBenchmark
+# echo "${src_code}" > /tmp/main.c
+# gcc /tmp/main.c -o /tmp/largeFileBenchmark
 
+## TODO: write a filesystem large file size ocupation benchmark
+
+##########################################
+## Starting filesystem speed benchmarks ##
+##########################################
+
+fio --name=random-write \
+    --directory=${MOUNT_POINT} \
+    --ioengine=posixaio \
+    --rw=randwrite \
+    --bs=4k \
+    --size=4g \
+    --numjobs=1 \
+    --iodepth=1 \
+    --runtime=60 \
+    --time_based \
+    --end_fsync=1 > fs_randwrite_1job_4Gfs_4Kblocks.dat
+
+fio --name=random-write \
+    --directory=${MOUNT_POINT} \
+    --ioengine=posixaio \
+    --rw=randwrite \
+    --bs=64k \
+    --size=256m \
+    --numjobs=16 \
+    --iodepth=16 \
+    --runtime=60 \
+    --time_based \
+    --end_fsync=1 > fs_randwrite_16job_256Mfs_64Kblocks.dat
+
+fio --name=random-write \
+    --directory=${MOUNT_POINT} \
+    --ioengine=posixaio \
+    --rw=randwrite \
+    --bs=1m \
+    --size=16g \
+    --numjobs=1 \
+    --iodepth=1 \
+    --runtime=60 \
+    --time_based \
+    --end_fsync=1 > fs_randwrite_1job_1Gfs_1Mblocks.dat
