75503ffea14babdf4cbf342704e27ce2dacd5946
Bruno <brunopolicarpo@hotmail.com>
2020-09-16 19:21:59 -0300

    Script now has a default configuration file on /root Script now detects correctly when the VPN session is not set

--- scripts/config-vpn.sh
+++ scripts/config-vpn.sh
@@ -1,14 +1,17 @@
 #!/bin/bash 
 
-LOG=/dev/stdout 
+VPN_LOCATIONS=/root/vpn-locations
+VPN_SESSION=${VPN_LOCATIONS}.session
 
-if [[ -f ${HOME}/.vpn-locations ]]; then
-	shuf ${HOME}/.vpn-locations | tee ${HOME}/.vpn-locations.session
+if [[ -f "${VPN_LOCATIONS}" ]]; then
+	echo "Setting VPN session file: ${VPN_SESSION} ... "
+	shuf "${VPN_LOCATIONS}" > ${VPN_SESSION}
 	
 	cur_loc=1
-	N_loc=`wc -l ${HOME}/.vpn-locations | awk '{ print $1 }'`
+	N_loc=`wc -l "${VPN_SESSION}" | awk '{ print $1 }'`
 else 
-	rm -f ~/.vpn-locations.session
+	echo "[INFO] No locations file found: ${VPN_LOCATIONS} "
+	rm -f ${VPN_SESSION}
 fi
 
 opt="$1"
@@ -16,20 +19,21 @@ opt="$1"
 case $opt in
 
 	start)
-		timeout=1
+		timeout=2
 		while :; do
-			if [[ -f ${HOME}/.vpn-locations.session ]]; then
-				location=`sed -n "${cur_loc}{p;q}" ${HOME}/.vpn-locations.session`
+			if [[ -f "${VPN_SESSION}" ]]; then
+				location=`sed -n "${cur_loc}{p;q}" "${VPN_SESSION}"`
 				cur_loc=$(( (cur_loc + 1) % N_loc ))
 				
 				echo "Location: ${location}"
 			else 
 				location=""
+				echo "No location set"
 			fi
 			windscribe connect ${location}
 			windscribe status | grep -q CONNECTED
 			if [[ $? -eq 0 ]]; then
-				echo "`date +%c`: windscribe connected" 2>&1 # >> $LOG
+				echo "`date +%c`: windscribe connected"
 
 				#msteams open
 				#sites open	
@@ -44,7 +48,7 @@ case $opt in
 				sleep ${timeout}
 				timeout=$(( timeout * 2 ))
 
-				if [ ${timeout} -gt 32 ]; then 
+				if [[ ${timeout} -gt 32 ]]; then 
 					echo "`date +%c`: too many retries. Aborting" 2>&1 # >> $LOG
 					exit -1
 				else 
@@ -55,16 +59,15 @@ case $opt in
 		;;
 
 	stop)
-		echo "Finishing VPN service" 2>&1 
+		echo "Finishing VPN service"
 		windscribe disconnect
 		sudo iptables -F OUTPUT
 		sudo iptables -P OUTPUT ACCEPT
-		msteams close
-		rm -f ${HOME}/.vpn-locations.session 
+		rm -f "${VPN_LOCATIONS}.session"
 		;;
 
 	*)
-		echo "Invalid option: \"$opt\"" 2>&1
+		echo "Invalid option: \"$opt\""
 		exit -1
 		;;
 esac
