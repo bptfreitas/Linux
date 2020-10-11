ecd9141d7877ee1533c2cd01019f7987b3ddb40c
Bruno <brunopolicarpo@hotmail.com>
2020-10-11 19:30:46 -0300

    Added some error checking

--- scripts/proxmox_functions.sh
+++ scripts/proxmox_functions.sh
@@ -13,8 +13,7 @@ PROXMOX_NODES[6]=proxmox7
 PROXMOX_NODES[7]=proxmox8 
 
 export PROXMOX_NODES
-export TOTAL_PROXMOX_NODES=${#PROXMOX_NODES[@]}
-export NODE_TO_MIGRATE=0 
+export NEXT_NODE_TO_MIGRATE=0 
 export VM_TO_CLONE=9004
 
 function proxmox_adduser(){
@@ -22,19 +21,27 @@ function proxmox_adduser(){
 	PASSWORD=$2
 	VM_ID=$3
 
+	total_proxmox_nodes=${#PROXMOX_NODES[@]}
+
+	[[ $# -ne 3 ]] &&\ 
+		{ echo "[ERROR] Invalid number of arguments: $#"; return -1 } 
+
+	[[ ${total_proxmox_nodes} -eq 0 ]] &&\
+		{ echo "[ERROR] proxmox nodes not defined "; return -1; }
+
 	echo "Creating VM ${VM_ID} for user '${USERNAME}'" >> ${LOG_ADDUSER} 
 	pvum useradd ${USERNAME} --password ${PASSWORD}
 	qm clone ${VM_TO_CLONE} ${VM_ID} 
 	if [[ $? -eq 0 ]]; then 
 		for i in `seq ${TOTAL_PROXMOX_NODES}`; do 
-			NODE_TO_MIGRATE=$(( (NODE_TO_MIGRATE + 1) % TOTAL_PROXMOX_NODES ))
-			echo "Migrating VM ${VM_ID} to node ${PROXMOX_NODES[$NODE_TO_MIGRATE]}" 
+			NEXT_NODE_TO_MIGRATE=$(( (NEXT_NODE_TO_MIGRATE + 1) % total_proxmox_nodes ))
+			echo "Migrating VM ${VM_ID} to node ${PROXMOX_NODES[$NEXT_NODE_TO_MIGRATE]}" 
 
-			qm migrate ${VM_ID} ${PROXMOX_NODES[$NODE_TO_MIGRATE]} 
+			qm migrate ${VM_ID} ${PROXMOX_NODES[$NEXT_NODE_TO_MIGRATE]} 
 			if [[ $? -eq 0 ]]; then 
 				echo "Migration concluded. Changing permissions to VM" >> ${LOG_ADDUSER} 
 				pveum aclmod /vms/${VM_ID} -user ${USERNAME}@pve -role AlunoCefet
-                export NODE_TO_MIGRATE
+                export NEXT_NODE_TO_MIGRATE
 				break 
 			else
 				if [[ $i -eq ${TOTAL_PROXMOX_NODES} ]]; then 
