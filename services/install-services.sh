d42dc7c30baded298ec0396312b9ba92bcbe0455
Bruno <brunopolicarpo@hotmail.com>
2021-12-25 12:10:48 -0300

    Rework of install-services to install and uninstall services one by one

--- services/install-services.sh
+++ services/install-services.sh
@@ -1,157 +1,82 @@
 #!/bin/bash
 
 DEBUG=1
+CONF_OUTPUT=0
 
-if [ $DEBUG -eq 1 ]; then 
-	action=reinstall
-else
-	action="$1"
+action="$1"
+service="$2"
+space="$3"
 
-	case $action in
-		install|uninstall)
-			;;
 
-		reinstall)
-			;;
-		
-		*)
-			echo "Invalid action: \"$action\" "
-			exit -1
-			;;
-	esac;
+case $action in
+	install|uninstall)	
+		;;
+	*)
+		echo "Invalid action: \"$action\" "
+		exit -1
+		;;
+esac;
+
+find . -name "$service";
+
+if [[ $? -ne 0 ]]; then
+	echo "[ERROR] Service '$service' not found"
+	exit -1
 fi
 
+case $space in
+	user|system)
+		;;
+	*)
+		echo "[ERROR] Invalid space: \"$action\". Must be 'user' or 'system' "
+		exit -1
+		;;
+esac;
 
-while [[ $# -gt 0 ]]
-do
-    key="$1"
-
-    case $key in
-		--install)
-			action="install"
-			shift # past argument
-			;;
-
-        --uninstall)
-            action="uninstall"
-            shift # past argument
-            ;; 
-
-		# reinstall
-        --reinstall)
-            CONF_OUTPUT=1
-            shift # past argument
-            ;;
-
-		# unknown option
-        *)    
-            POSITIONAL+=("$1") # save it in an array for later
-            shift # past argument
-            ;;
-    esac
-done
+#        *)    
+#            POSITIONAL+=("$1") # save it in an array for later
+#            shift # past argument
+#            ;;
 
 curdir=`pwd`
 rootdir=`dirname ${curdir}`
-services_lst=/tmp/services.lst
 
 echo "Root dir: ${rootdir}"
 
-sed -e 's/#.*//' services.conf > /tmp/services.conf.tmp
+echo ">>> STAGE 1: Stopping and disabling '${service}' <<<"
 
-> ${services_lst}
+sudo systemctl stop ${service}
+sudo systemctl disable ${service}
 
-for service_conf in $(cat /tmp/services.conf.tmp); do
+systemctl --user stop ${service}
+systemctl --user disable ${service}
 
-	service=`echo "${service_conf}" | cut -f1 -d:`
-	space=`echo "${service_conf}" | cut -f2 -d:`
+sudo rm -f "/etc/systemd/user/${service}"
+sudo rm -f "/etc/systemd/system/${service}"
 
-	echo "${service}" >> /tmp/services.lst
+echo ">>> STAGE 2: Installing \"${service}\" on \"${space}\" space <<< "
 
-	echo "Stopping and disabling '${service}' on '${space}' space... "
+sed -e "s/SERVICES_FOLDER/${rootdir//\//\\\/}/g" ${service} > ./${service}.tmp
 
-	[ "${space}" == "user" ] && opt_USER=--user || opt_USER=""
-	[ "${space}" == "system" ] && opt_SUDO=sudo || opt_SUDO=""
+sudo mv ./${service}.tmp /etc/systemd/${space}/${service}
 
-	${opt_SUDO} systemctl ${opt_USER} stop ${service}
-	${opt_SUDO} systemctl ${opt_USER} disable ${service};
-
-	echo "Installing \"${service}\" on \"${space}\" space ... "
-
-	sed -e "s/SERVICES_FOLDER/${rootdir//\//\\\/}/g" ${service} > /tmp/${service}.tmp
-
-	case ${space} in
-		system|user)
-			sudo mv /tmp/${service}.tmp /etc/systemd/${space}/${service}
-			;;
-
-		*)
-			echo "Error! invalid space for service installation: ${space}"
-			;;
-	esac
-done
-
-echo "Reloading daemons ..."
+echo ">>> STAGE 3: Reloading daemons <<< "
 
 sudo systemctl daemon-reload
 systemctl --user daemon-reload
 
-for service_conf in $(cat /tmp/services.conf.tmp); do
-	service=`echo "${service_conf}" | cut -f1 -d:`
-	space=`echo "${service_conf}" | cut -f2 -d:`
+echo ">>> STAGE 4: Enabling service <<< "
 
-	echo "Enabling ${service} ... "]
+case $space in
 
-	[ "${space}" == "user" ] && opt_USER=--user || opt_USER=""
-	[ "${space}" == "system" ] && opt_SUDO=sudo || opt_SUDO=""
+	user)
+		systemctl --user enable ${service};
+		;;
 
-	${opt_SUDO} sudo systemctl ${opt_USER} enable ${service};
-done
-
-for service_conf in $(cat /tmp/services.conf.tmp); do
-	service=`echo "${service_conf}" | cut -f1 -d:`
-	space=`echo "${service_conf}" | cut -f2 -d:`
-
-	echo "Starting ${service} ... "
-
-	[ "${space}" == "user" ] && opt_USER=--user || opt_USER=""
-	[ "${space}" == "system" ] && opt_SUDO=sudo || opt_SUDO=""	
-
-	${opt_SUDO} systemctl ${opt_USER} start ${service};
-done
-
-rm ${services_lst}
-rm /tmp/services.conf.tmp
-
-# if [ ! -f "$PWD/git-sync.service" ]; then
-# 	echo "git-sync.service not found - aborting"
-# 	exit
-# fi
-
-# if [ ! -f "$PWD/git-sync.py" ]; then
-# 	echo "git-sync.py not found - aborting"
-# 	exit
-# fi
-
-# if [ ! -f "$PWD/git-repositories" ]; then
-# 	echo "git-repositories not found - aborting"
-# 	exit
-# fi
-
-# if [ -L /etc/systemd/system/git-sync.service ]; then
-# 	unlink /etc/systemd/system/git-sync.service
-# fi
-# if [ -L /usr/local/bin/git-sync.py ]; then
-# 	unlink /usr/local/bin/git-sync.py
-# fi
-# if [ -L $HOME/.git-repositories ]; then
-# 	unlink $HOME/.git-repositories
-# fi
-
-# ln -s $PWD/git-sync.service /etc/systemd/system/git-sync.service
-
-# ln -s $PWD/git-sync.py /usr/local/bin/git-sync.py
-
-# chmod +x /usr/local/bin/git-sync.py
-
-# ln -s $PWD/git-repositories $HOME/.git-repositories
+	system)
+		sudo systemctl enable ${service};
+		;;
+	*)
+		echo "Invalid space for installation. Must be 'system' or 'user'"
+		;;
+esac;
